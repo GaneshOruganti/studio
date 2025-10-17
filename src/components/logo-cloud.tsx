534593c0b8ede44ce2d4f187c961ada51fef5453
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-10-17 21:38:21 +0000

    below our record , add this animation, <div class="slider"> <div class="

--- src/components/logo-cloud.tsx
+++ src/components/logo-cloud.tsx
@@ -4,13 +4,13 @@
 import Image from 'next/image';
 
 const logos = [
-  { src: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg", alt: "Google" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.svg", alt: "Meta" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", alt: "Amazon" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Microsoft_logo_%282012%29.svg", alt: "Microsoft" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg", alt: "Apple" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/9/95/Salesforce.com_logo.svg", alt: "Salesforce" },
-  { src: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/TATA_Logo.svg/1280px-TATA_Logo.svg.png", alt: "Tata" },
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png",
 ];
 
 const duplicatedLogos = [...logos, ...logos];
@@ -19,14 +19,14 @@ export function LogoCloud() {
   return (
     <div className="logo-slider">
       <div className="logo-slide-track">
-        {duplicatedLogos.map((logo, index) => (
+        {duplicatedLogos.map((logoSrc, index) => (
           <div key={index} className="logo-slide">
             <Image 
-              src={logo.src} 
+              src={logoSrc} 
               height="100" 
-              width="150" 
-              alt={logo.alt}
-              className="object-contain grayscale hover:grayscale-0 transition-all duration-300 opacity-60 hover:opacity-100" 
+              width="250" 
+              alt={`Client Logo ${index + 1}`}
+              className="object-contain" 
             />
           </div>
         ))}
