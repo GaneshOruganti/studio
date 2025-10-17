18982a126083433d506e75aedc046a8c54f23a49
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-10-17 19:29:47 +0000

    https://i.postimg.cc/HWQ6Y1W8/Red-White-Simple-Company-Technology-Logo-1

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,4 @@
+
 import type { SVGProps } from "react";
 
 export const Icons = {
@@ -8,21 +9,9 @@ export const Icons = {
       fill="none"
       {...props}
     >
-      <rect width="100" height="100" rx="12" fill="#2563EB" />
-      <path
-        d="M20 20C20 14.4772 24.4772 10 30 10H80V90H30C24.4772 90 20 85.5228 20 80V20Z"
-        fill="white"
-      />
-      <path
-        d="M80 20C80 14.4772 75.5228 10 70 10H20V90H70C75.5228 90 80 85.5228 80 80V20Z"
-        fill="white"
-        transform="rotate(180 50 50)"
-      />
-      <path
-        d="M50 30L55.8779 44.1221L70 50L55.8779 55.8779L50 70L44.1221 55.8779L30 50L44.1221 44.1221L50 30Z"
-        fill="#2563EB"
-        transform="rotate(20 50 50)"
-      />
+      <circle cx="50" cy="50" r="50" fill="#EF4444"/>
+      <path d="M62.5 37.5C62.5 30.5964 56.9036 25 50 25C43.0964 25 37.5 30.5964 37.5 37.5V43.75C37.5 47.2018 34.7018 50 31.25 50H25" stroke="white" strokeWidth="10" strokeLinecap="round"/>
+      <path d="M37.5 62.5C37.5 69.4036 43.0964 75 50 75C56.9036 75 62.5 69.4036 62.5 62.5V56.25C62.5 52.7982 65.2982 50 68.75 50H75" stroke="white" strokeWidth="10" strokeLinecap="round"/>
     </svg>
   ),
 };
