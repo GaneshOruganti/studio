a5367433888aa697bcc1902bc24b1c311af79063
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-10-17 15:07:49 +0000

    i waht the same design in favcon and aslo in the headr logo before the l

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&family=Poppins:wght@700&display=swap" rel="stylesheet" />
