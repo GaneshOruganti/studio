e2efb1df604ecdcb6a995136fad55f66e6a2c94e
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-10-24 08:01:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3331741041-7d41f",
+  "appId": "1:416259817765:web:038a2a320e4b6a4b6edacc",
+  "apiKey": "AIzaSyAiW4LlmYbg3orU-iHhgN2QQreRzdYHUck",
+  "authDomain": "studio-3331741041-7d41f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "416259817765"
+};
