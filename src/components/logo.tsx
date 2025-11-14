da5ac9c70d14f080e6d36c7e1f580253e508f821
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-11-14 19:52:11 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import Image from "next/image";
-
 export function Logo() {
   const logoSrc = "https://i.postimg.cc/pLk4JhpS/Red-White-Simple-Company-Technology-Logo-5.png";
 
   return (
-    <Image 
+    <img 
       src={logoSrc} 
       alt="Speedopus Logo" 
       width={160} 
       height={40} 
-      unoptimized
       className="h-8 w-auto"
     />
   );
