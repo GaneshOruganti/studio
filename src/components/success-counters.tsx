d8066246feeb5839a236e27e7f836825eabaf4cf
Ganesh Oruganti <ganeshoruganti2022@gmail.com>
2025-11-14 20:16:02 +0000

    in mobile view , our track recod is not visible , make them in like like

--- src/components/success-counters.tsx
+++ src/components/success-counters.tsx
@@ -71,7 +71,7 @@ const Counter = ({ label, value, suffix, isRating }: { label: string, value: num
 
 export function SuccessCounters() {
     return (
-        <div className="grid grid-cols-2 gap-y-8 gap-x-4 sm:grid-cols-3 md:grid-cols-5 md:gap-8">
+        <div className="grid grid-cols-2 gap-y-8 gap-x-4 sm:grid-cols-3 lg:grid-cols-5 md:gap-8">
             {counters.map(counter => (
                 <Counter key={counter.label} {...counter} />
             ))}
